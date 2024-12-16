f42c8f8ddd1f97c31de540ae693a0cf085c8c90b
zaman-37 <kamruzzaman224125@gmail.com>
2024-12-17 03:33:41 +0600

    new file added

---
+++ code/cp_template02.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+#define tt int t; cin >> t; for (int tc=1; tc<=t; tc++)
+#define disp(res) cout << "Case " << tc << ": " << (res) << endl
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+
+
+int main() {
+    optimize()
+    
+
+    return 0;
+}
